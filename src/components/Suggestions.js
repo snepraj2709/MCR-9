0e25e7c451b9786bb5cc0463ed2e8b7a1575e04a
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-07-29 19:56:13 +0530

    feat: Added Suggestion bar of more videos

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useData } from "../context/DataContext";
+import VideoCard from "./VideoCard";
 
 function Suggestions() {
+  const { state } = useData();
   return (
-    <div className="hidden md:col-span-2 md:inline-block">More Videos</div>
+    <div className="hidden md:col-span-2 md:inline-block">
+      <h2 className="text-base font-bold text-center">More Videos</h2>
+      {state?.allVideos?.map((video) => (
+        <div key={video._id}>
+          <VideoCard data={video} />
+        </div>
+      ))}
+    </div>
   );
 }
 
